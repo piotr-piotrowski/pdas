f4cdb3461f82819a887f653d4461344396b2fefa
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2023-02-23 13:15:16 +0100

    feat: styling with bulma and add description property

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,9 +1,19 @@
-function ProfileCard({ title, handle, image }) {
+function ProfileCard({ title, handle, image, description }) {
   return (
-    <div>
-      <img src={image} alt={title} />
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt={title} />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">Title is {title}</p>
+          <p className="subtitle is-6">Handle is {handle}</p>
+        </div>
+        <div className="content">{description}</div>
+      </div>
     </div>
   );
 }
